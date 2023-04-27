5444faeea330ab674a5349b124ef0fe0704f50db
Anil Gurau <gurau.anil@gmail.com>
2023-04-28 00:52:16 +0545

    configuring app configuration nad services

--- HangFireWithDotnet6/Program.cs
+++ HangFireWithDotnet6/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var builder = new HostBuilder()
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile(@"appsettings.json", optional: false, reloadOnChange: true);
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        // Retrieve the configuration settings from the configuration system
+        var configuration = hostContext.Configuration;
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+    })
+    .UseConsoleLifetime();
+
+await builder.RunConsoleAsync();
