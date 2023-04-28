07ec0be0b55b8572850f2ce09ea275dd7739e3ea
Anil Gurau <gurau.anil@gmail.com>
2023-04-29 00:25:51 +0545

    implementing background job

--- HangFireWithDotnet6/App.cs
+++ HangFireWithDotnet6/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hangfire;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,16 @@ namespace HangFireWithDotnet6
 {
     public class App
     {
+        private readonly IBackgroundJobClient _backgroundJobs;
+        public App(IBackgroundJobClient backgroundJobs)
+        {
+            _backgroundJobs = backgroundJobs;
+        }
         public void Main()
         {
-            Console.WriteLine("Hello World");
+            _backgroundJobs.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+            _backgroundJobs.Schedule(() => Console.WriteLine("Hello world from Hangfire test!"), TimeSpan.FromMinutes(1));
+            RecurringJob.AddOrUpdate("easyjob", () => Console.Write("Easy!"), Cron.Minutely);
         }
     }
 }
